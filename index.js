0a2afd1e5acbc4e922eb49d3e9cbd63e6d022f4c
Juan Ignacio Rodriguez Varela <nacho.rv2304@gmail.com>
2022-05-27 16:44:04 -0600

    Se realiza el código javaScript de la sección de testimonio.

---
+++ index.js
@@ -0,0 +1,75 @@
+var contador = 1;
+
+const testimonio = () => {
+
+    let flechaAdelante = document.getElementById("flechaAdelante");
+
+    let flechaAtras = document.getElementById("flechaAtras");
+
+    let testimonio1 = document.getElementById("testimonio1");
+
+    let testimonio2 = document.getElementById("testimonio2");
+
+    let testimonio3 = document.getElementById("testimonio3");
+
+    flechaAtras.addEventListener("click", () => {
+        switch (contador) {
+
+            //Oculta testimonio1, Muestra testimonio3.
+            case 1:
+                testimonio1.classList.toggle("testimonio-oculto");
+                testimonio3.classList.toggle("testimonio-oculto");
+
+                contador = 3;
+            break;
+
+            //Oculta testimonio2, Muestra testimonio1.
+            case 2:
+                testimonio2.classList.toggle("testimonio-oculto");
+                testimonio1.classList.toggle("testimonio-oculto");
+
+                contador = 1;
+            break;
+
+            //Oculta testimonio3, Muestra testimonio2.
+            case 3:
+                testimonio3.classList.toggle("testimonio-oculto");
+                testimonio2.classList.toggle("testimonio-oculto");
+
+                contador = 2;
+            break;
+        }
+    })
+
+    flechaAdelante.addEventListener("click", () => {
+
+        switch (contador) {
+
+            //Oculta testimonio1, Muestra testimonio2.
+            case 1:
+                testimonio1.classList.toggle("testimonio-oculto");
+                testimonio2.classList.toggle("testimonio-oculto");
+
+                contador++;
+            break;
+
+            //Oculta testimonio2, Muestra testimonio3.
+            case 2:
+                testimonio2.classList.toggle("testimonio-oculto");
+                testimonio3.classList.toggle("testimonio-oculto");
+                
+                contador++;
+            break;
+
+            //Oculta testimonio3, Muestra testimonio1.
+            case 3:
+                testimonio3.classList.toggle("testimonio-oculto");
+                testimonio1.classList.toggle("testimonio-oculto");
+
+                contador = 1;
+            break;
+        }
+    });
+}
+
+testimonio();
